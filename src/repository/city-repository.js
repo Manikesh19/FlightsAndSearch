23e68de734987bc001bfb4a391bb563643e6b2b3
Manikesh <manikesh19dec@gmail.com>
2024-11-20 01:05:29 +0530

    Added more routers for city Api

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -41,7 +41,7 @@ class CityRepository {
 
     async getCity(cityId) {
         try {
-            const city = await City.findbyPk(cityId);
+            const city = await City.findByPk(cityId);
             return city;
         } catch (error) {
             console.log("Something went wrong in the repository layer.");
